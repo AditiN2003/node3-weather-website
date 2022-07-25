257eb689ba3e08385c6f1faec9bfd6c06d414972
Aditi Nachnani <anachnani@quadyster.com>
2022-07-25 11:21:40 -0500

    added to forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,8 @@
 const request = require('request')
 
+
+
+
 const forecast = (latitude, longitude, callback) => {
     const url ='http://api.weatherstack.com/current?access_key=f8daf6229de5a7a1bd56cc8dc345e677&query=' + latitude+','+longitude+'&units=f'
     request ({url,json:true}, (error,{body}) => {
@@ -8,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Wrong input', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' )
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. ' + 'The wind speed is ' + body.current.wind_speed + '.')
         }
     })
 }
